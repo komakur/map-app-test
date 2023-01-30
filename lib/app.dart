daf03672c6c0d49ff82eb2fc07a6abb1232d47db
kostvick <my.mail.kostya@gmail.com>
2023-01-30 15:20:45 +0200

    add: provide authentication repository and bloc

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:map_app_test/auth/bloc/auth_bloc.dart';
+import 'package:map_app_test/data/repositories/auth_repository.dart';
+
+import 'package:map_app_test/auth/view/pages/sign_up_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => AuthRepository(),
+      child: BlocProvider(
+        create: (context) => AuthBloc(
+          authRepository: RepositoryProvider.of<AuthRepository>(context),
+        ),
+        child: MaterialApp(
+          theme: ThemeData.light(),
+          home: const SignUpPage(),
+        ),
+      ),
+    );
+  }
+}
