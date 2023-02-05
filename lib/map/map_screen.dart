5bc44b0a2c7759ac1f13864f3e9ca84250c9ecc1
kostvick <my.mail.kostya@gmail.com>
2023-02-05 05:10:40 +0200

    add: to current position button

--- lib/map/map_screen.dart
+++ lib/map/map_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:map_app_test/geolocation/bloc/geolocaiton_bloc.dart';
+import 'package:map_app_test/geolocation/location_service.dart';
 import 'package:map_app_test/profile_sidebar/profile_sidebar.dart';
 import 'package:map_app_test/user/bloc/user_bloc.dart';
 import 'package:user_repository/user_repository.dart';
@@ -19,7 +20,9 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   StreamSubscription? _getPositionSubscription;
+  final _searchController = TextEditingController();
   User? _user;
+  GoogleMapController? _mapController;
 
   List<Marker> _markersFromUsers(List<User> users) => users
       .map<Marker>(
@@ -30,6 +33,16 @@ class _MapPageState extends State<MapPage> {
         ),
       )
       .toList();
+
+  Future<void> _goToUser() async {
+    print('animation');
+    final currentPosition = await Geolocator.getCurrentPosition();
+    _mapController?.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+          target: LatLng(currentPosition.latitude, currentPosition.longitude),
+          zoom: 10),
+    ));
+  }
 
   @override
   void initState() {
@@ -49,7 +62,9 @@ class _MapPageState extends State<MapPage> {
 
   @override
   void dispose() {
-    _getPositionSubscription!.cancel();
+    _getPositionSubscription?.cancel();
+    _searchController.dispose();
+    _mapController?.dispose();
     super.dispose();
   }
 
@@ -58,44 +73,80 @@ class _MapPageState extends State<MapPage> {
     return Scaffold(
       key: _scaffoldKey,
       drawer: ProfileSidebar(user: _user!),
+      appBar: AppBar(title: const Text('Find user app')),
       body: BlocBuilder<GeolocaitonBloc, GeolocaitonState>(
         builder: (context, geolocationState) {
           if (geolocationState is GeolocaitonLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (geolocationState is GeolocaitonLoaded) {
-            return Stack(
+            return Column(
               children: [
-                BlocBuilder<UserBloc, UserState>(
-                  builder: (context, state) {
-                    if (state is UserLoading) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (state is UsersLoaded) {
-                      List<User> users = state.users;
-                      final markers = _markersFromUsers(users);
-                      return GoogleMap(
-                        myLocationEnabled: true,
-                        myLocationButtonEnabled: true,
-                        initialCameraPosition: CameraPosition(
-                            target: LatLng(geolocationState.position.latitude,
-                                geolocationState.position.longitude),
-                            zoom: 5),
-                        markers: markers.toSet(),
-                      );
-                    }
-                    return const Center(child: Text('Something went wrong'));
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: IconButton(
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _searchController,
+                        textCapitalization: TextCapitalization.words,
+                        decoration:
+                            const InputDecoration(hintText: 'Search the city'),
+                        onChanged: (value) => print(value),
+                      ),
+                    ),
+                    IconButton(
                       onPressed: () {
-                        _scaffoldKey.currentState?.openDrawer();
+                        LocationService().getPlace(_searchController.text);
                       },
-                      icon: const Icon(
-                        Icons.arrow_circle_right,
-                        color: Colors.amberAccent,
-                      )),
+                      icon: const Icon(Icons.search),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      BlocBuilder<UserBloc, UserState>(
+                        builder: (context, state) {
+                          if (state is UserLoading) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          }
+                          if (state is UsersLoaded) {
+                            List<User> users = state.users;
+                            final markers = _markersFromUsers(users);
+                            return GoogleMap(
+                              myLocationEnabled: true,
+                              myLocationButtonEnabled: false,
+                              zoomControlsEnabled: false,
+                              zoomGesturesEnabled: true,
+                              onMapCreated: (controller) {
+                                setState(() {
+                                  _mapController = controller;
+                                });
+                              },
+                              initialCameraPosition: CameraPosition(
+                                  target: LatLng(
+                                      geolocationState.position.latitude,
+                                      geolocationState.position.longitude),
+                                  zoom: 5),
+                              markers: markers.toSet(),
+                            );
+                          }
+                          return const Center(
+                              child: Text('Something went wrong'));
+                        },
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: IconButton(
+                            onPressed: () {
+                              _scaffoldKey.currentState?.openDrawer();
+                            },
+                            icon: const Icon(
+                              Icons.arrow_circle_right,
+                              color: Colors.amberAccent,
+                            )),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             );
@@ -104,6 +155,12 @@ class _MapPageState extends State<MapPage> {
               child: Text('Something went wrong'),
             );
           }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.my_location),
+        onPressed: () async {
+          await _goToUser();
         },
       ),
     );
